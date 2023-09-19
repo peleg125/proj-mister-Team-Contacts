775a5117af38dba39d2072b364a469bbb6b01634
peleg125 <peleg@mischel.biz>
2023-09-19 15:07:06 +0300

    fixoed template

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,28 +2,20 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
-import { AppHeader } from './cmps/AppHeader.jsx'
-import { AppFooter } from './cmps/AppFooter.jsx'
-
 import { HomePage } from './pages/HomePage.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
-import { store } from './store/store.js'
 
 export function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <section className='main-layout app'>
-          <AppHeader />
-          <main>
-            <Routes>
-              <Route element={<HomePage />} path='/' />
-              <Route element={<AboutUs />} path='/about' />
-            </Routes>
-          </main>
-          <AppFooter />
-        </section>
-      </Router>
-    </Provider>
+    <Router>
+      <section className='main-layout app'>
+        <main>
+          <Routes>
+            <Route element={<HomePage />} path='/' />
+            <Route element={<AboutUs />} path='/about' />
+          </Routes>
+        </main>
+      </section>
+    </Router>
   )
 }
