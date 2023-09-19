d0ebf2ba1c75f15f80ee5ebce8462f02c2ade5fc
peleg125 <peleg@mischel.biz>
2023-09-19 17:39:16 +0300

    added functionality to contactDetails
     and added UserMsg

--- pages/ContactDetails.jsx
+++ pages/ContactDetails.jsx
@@ -1,7 +1,8 @@
 const { useState, useEffect } = React
 const { useParams, useNavigate } = ReactRouterDOM
-
 import { contactService } from '../services/contact.service.js'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { removeContact } from '../store/actions/contact.actions.js'
 
 export function ContactDetails() {
   const params = useParams()
@@ -11,7 +12,7 @@ export function ContactDetails() {
 
   useEffect(() => {
     contactService
-      .getById(params.id)
+      .getById(params.contactId)
       .then((contact) => {
         if (!contact) return navigate('/contact')
         setCurrContact(contact)
@@ -20,6 +21,18 @@ export function ContactDetails() {
         console.log('Had issues loading contact', err)
       })
   }, [])
+
+  function onRemoveContact() {
+    removeContact(params.contactId)
+      .then(() => {
+        showSuccessMsg('Contact removed')
+        navigate('/contact')
+      })
+      .catch((err) => {
+        console.log('Cannot remove contact', err)
+        showErrorMsg('Cannot remove contact')
+      })
+  }
 
   if (!currContact) return <h4>loading</h4>
   const { _id, firstName, lastName, email, phone, desc } = currContact
@@ -33,17 +46,21 @@ export function ContactDetails() {
         <h2>email: {email}</h2>
         <h2>phone: {phone}</h2>
         <h2>description: {desc}</h2>
+        <button className='del-btn' onClick={() => onRemoveContact()}>
+          Delete Contact
+        </button>
         <button className='back-btn' onClick={() => navigate('/contact')}>
           Back to contacts
         </button>
+        <button
+          className='nav-edit-btn'
+          onClick={() => {
+            navigate(`/contact/edit/${params.contactId}`)
+          }}
+        >
+          Edit
+        </button>
       </div>
     </section>
-    // <section>
-    //   <h1>Contact Name</h1>
-    //   <h3>contact info</h3>
-    //   <p>contact number</p>
-    //   <p>contact email</p>
-    //   <p>contact </p>
-    // </section>
   )
 }
