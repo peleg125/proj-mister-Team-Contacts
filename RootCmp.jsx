38dac033d8a8a5b98eae5aa2f1cf1dbfed37a0c5
peleg125 <peleg@mischel.biz>
2023-09-19 18:03:46 +0300

    F/ContactEdit/Added edit functionality

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,12 +2,14 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
-import { HomePage } from "./pages/HomePage.jsx"
-import { AboutUs } from "./pages/AboutUs.jsx"
-import { ContactDetails } from "./pages/ContactDetails.jsx"
-import { ContactIndex } from "./pages/ContactIndex.jsx"
-import { store } from "./store/store.js"
-import { AppHeader } from "./cmps/AppHeader.jsx"
+import { HomePage } from './pages/HomePage.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { ContactDetails } from './pages/ContactDetails.jsx'
+import { ContactIndex } from './pages/ContactIndex.jsx'
+
+import { store } from './store/store.js'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { ContactEdit } from './pages/ContactEdit.jsx'
 
 export function App() {
   return (
@@ -19,6 +21,7 @@ export function App() {
             <Routes>
               <Route element={<HomePage />} path='/' />
               <Route element={<ContactDetails />} path='/contact/details/:contactId' />
+              <Route element={<ContactEdit />} path='/contact/edit/:contactId' />
               <Route element={<ContactIndex />} path='/contact' />
               <Route element={<AboutUs />} path='/about' />
             </Routes>
---
+++ pages/ContactEdit.jsx
@@ -0,0 +1,76 @@
+import { contactService } from '../services/contact.service.js'
+import { showErrorMsg } from '../services/event-bus.service.js'
+
+const { useEffect, useState } = React
+const { useParams, useNavigate } = ReactRouterDOM
+
+export function ContactEdit() {
+  const [contactToEdit, setContactToEdit] = useState(contactService.getEmptyContact())
+  const navigate = useNavigate()
+  const params = useParams()
+
+  useEffect(() => {
+    if (params.contactId) loadContact()
+  }, [])
+
+  function loadContact() {
+    contactService
+      .getById(params.contactId)
+      .then(setContactToEdit)
+      .catch((err) => {
+        console.log('Had issued in contact edit:', err)
+        navigate('/contact')
+        showErrorMsg('Contact not found!')
+      })
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setContactToEdit({
+      ...contactToEdit,
+      [name]: value,
+    })
+  }
+
+  function onSaveContact(ev) {
+    ev.preventDefault()
+    contactService
+      .save(contactToEdit)
+      .then(() => navigate('/contact'))
+      .catch((err) => {
+        showErrorMsg('Cannot save contact', err)
+      })
+  }
+
+  const { firstName, lastName, email, phone, desc } = contactToEdit
+
+  return (
+    <section className='contact-edit'>
+      <h2>Edit Contact</h2>
+
+      <form onSubmit={onSaveContact}>
+        <div>
+          <label>First Name</label>
+          <input type='text' name='firstName' value={firstName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type='text' name='lastName' value={lastName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type='email' name='email' value={email} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Phone</label>
+          <input type='tel' name='phone' value={phone} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Description</label>
+          <textarea name='description' value={desc} onChange={handleChange} />
+        </div>
+        <button type='submit'>{params.contactId ? 'Update' : 'Add'} Contact</button>
+      </form>
+    </section>
+  )
+}
