4c497c44085c118922153b36e6b6727c85df7c3a
peleg125 <peleg@mischel.biz>
2023-09-19 16:55:14 +0300

    added provider

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -2,26 +2,27 @@ const Router = ReactRouterDOM.HashRouter
 const { Route, Routes } = ReactRouterDOM
 const { Provider } = ReactRedux
 
-
 import { HomePage } from './pages/HomePage.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { ContactDetails } from './pages/ContactDetails.jsx'
-import { ContactIndex } from "./pages/ContactIndex.jsx"
-
+import { ContactIndex } from './pages/ContactIndex.jsx'
+import { store } from './store/store.js'
 
 export function App() {
   return (
-    <Router>
-      <section className='main-layout app'>
-        <main>
-          <Routes>
-            <Route element={<HomePage />} path='/' />
-            <Route element={<ContactDetails />} path='/contact/details/:contactId' />
-            <Route element={<ContactIndex />} path='/contact' />   
-            <Route element={<AboutUs />} path='/about' />
-          </Routes>
-        </main>
-      </section>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <section className='main-layout app'>
+          <main>
+            <Routes>
+              <Route element={<HomePage />} path='/' />
+              <Route element={<ContactDetails />} path='/contact/details/:contactId' />
+              <Route element={<ContactIndex />} path='/contact' />
+              <Route element={<AboutUs />} path='/about' />
+            </Routes>
+          </main>
+        </section>
+      </Router>
+    </Provider>
   )
 }
