f2066464b7efc5ceb44a017597ef9177a7e1755f
OriDalm <oridalmedigos@gmail.com>
2023-09-19 18:58:26 +0300

    Designed contact list

--- cmps/ContactPreview.jsx
+++ cmps/ContactPreview.jsx
@@ -1,10 +1,10 @@
 export function ContactPreview({ contact }) {
   return (
-    <article className='contact=preview'>
-      {contact.firstName}
-      {contact.lastName}
-      {contact.email}
-      {contact.phone}
+    <article className='contact-preview'>
+      <div className='contact-name'>
+        {contact.firstName} {contact.lastName}
+      </div>
+      <div className='contact-phone'>{contact.phone}</div>
     </article>
   )
 }
